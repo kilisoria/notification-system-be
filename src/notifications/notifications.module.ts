3b8e26cb1be125219d33b3682061fa78e178847e
Cristians Soria <cristiansoria@gmail.com>
2023-06-07 01:18:37 -0300

    Add notification schema

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { NotificationSchema } from './schemas/notifications.schema';
+
 import { NotificationsService } from './notifications.service';
 
 @Module({
-  providers: [NotificationsService]
+  imports: [
+    // HttpModule,
+    MongooseModule.forFeature([
+      {
+        name: 'Notification',
+        schema: NotificationSchema,
+        collection: 'notifications',
+      },
+    ]),
+  ],
+  providers: [NotificationsService],
 })
 export class NotificationsModule {}
--- src/notifications/schemas/notifications.schema.ts
+++ src/notifications/schemas/notifications.schema.ts
@@ -1,7 +1,26 @@
-import { Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
+
+import { User } from 'src/users/schemas/user.schema';
+import { Channel } from 'src/channels/schemas/channel.schema';
+import { NotificationCategory } from 'src/notification-categories/schemas/notification-category.schema';
 
 @Schema()
 export class Notification extends Document {
+  @Prop()
+  message: string;
 
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: User;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Channel' })
+  channel: Channel;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'NotificationCategory' })
+  subscribed: NotificationCategory;
+
+  @Prop()
+  sentAt: number;
 }
+
+export const NotificationSchema = SchemaFactory.createForClass(Notification);
